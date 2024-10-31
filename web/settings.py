0e89a11f7364ec89aea51400ecb0ca0af00e81dc
kawser11223 <148429350+kawser11223@users.noreply.github.com>
2024-10-31 11:58:36 +0600

    Update static files settings for Vercel deployment

--- web/settings.py
+++ web/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware'
+    
 ]
 
 ROOT_URLCONF = 'web.urls'
